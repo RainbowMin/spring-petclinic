45127498910f64bcd37526687b75c8aba0ac2c0e
闵杰(Rainbow) <minjie@hypergryph.com>
2026-02-11 15:51:08 +0800

    代码中动态创建Project

---
+++ .teamcity/DynamicCreateBuild/Project.kt
@@ -0,0 +1,37 @@
+package DynamicCreateBuild
+import jetbrains.buildServer.configs.kotlin.BuildType
+import jetbrains.buildServer.configs.kotlin.DslContext
+import jetbrains.buildServer.configs.kotlin.Project
+import jetbrains.buildServer.configs.kotlin.buildSteps.maven
+
+val operatingSystems = listOf("Mac OS X", "Windows", "Linux")
+val jdkVersions = listOf("JDK_18", "JDK_11")
+
+object Project : Project({
+    for (os in operatingSystems) {
+        for (jdk in jdkVersions) {
+            buildType(Build(os, jdk))
+        }
+    }
+})
+
+class Build(val os: String, val jdk: String) : BuildType({
+    id("Build_${os.replace(" ", "_")}_${jdk}")
+    name = "Build ($os, $jdk)"
+
+    vcs {
+        root(DslContext.settingsRoot)
+    }
+
+    steps {
+        maven {
+            goals = "clean package"
+            mavenVersion = defaultProvidedVersion()
+            jdkHome = "%env.${jdk}%"
+        }
+    }
+
+    requirements {
+        equals("teamcity.agent.jvm.os.name", os)
+    }
+})
